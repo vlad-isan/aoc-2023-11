9d9903ee71b898c9484fecbe1d799976a7e4205b
Vlad Isan <vlad@innerbyte.uk>
2023-12-11 16:44:03 +0000

    part 2

--- puzzle.cpp
+++ puzzle.cpp
@@ -20,7 +20,7 @@ int puzzle_sample_1(const std::string &base_file_path) {
     return do_puzzle_1(file);
 }
 
-int puzzle_sample_2(const std::string &base_file_path) {
+uint64_t puzzle_sample_2(const std::string &base_file_path) {
     std::string file_path = fmt::format("{}/{}", base_file_path, "puzzle-input-sample-2.txt");
 
     std::ifstream file(file_path);
@@ -52,7 +52,7 @@ int puzzle_1(const std::string &base_file_path) {
     return do_puzzle_1(file);
 }
 
-int puzzle_2(const std::string &base_file_path) {
+uint64_t puzzle_2(const std::string &base_file_path) {
     std::string file_path = fmt::format("{}/{}", base_file_path, "puzzle-input-2.txt");
 
     std::ifstream file(file_path);
@@ -88,14 +88,24 @@ int do_puzzle_1(std::ifstream &file) {
     return sum_shortest_distances(grid);
 }
 
-int do_puzzle_2(std::ifstream &file) {
+uint64_t do_puzzle_2(std::ifstream &file) {
     std::string line;
 
+    std::vector<std::vector<int>> grid;
+    int galaxy_idx = 0;
     while (std::getline(file, line)) {
-        fmt::println("{}", line);
+        std::vector<int> row;
+
+        for (auto &c: line) {
+            row.push_back(c == '#' ? ++galaxy_idx : 0);
+        }
+
+        grid.push_back(row);
     }
 
-    return 0;
+    auto expansion_points = find_expansion_points(grid);
+
+    return sum_shortest_distances(grid, expansion_points);
 }
 
 void expand_universe(std::vector<std::vector<int>> &grid) {
@@ -152,6 +162,62 @@ int sum_shortest_distances(std::vector<std::vector<int>> &grid) {
             auto [row2, col2] = galaxy_positions[j];
 
             sum += std::abs(row1 - row2) + std::abs(col1 - col2);
+        }
+    }
+
+    return sum;
+}
+
+std::pair<std::vector<size_t>, std::vector<size_t>> find_expansion_points(const std::vector<std::vector<int>> &grid) {
+    std::pair<std::vector<size_t>, std::vector<size_t>> expansion_points{};
+
+    // find empty rows
+    for (int i = 0; i < grid.size(); ++i) {
+        if (std::ranges::all_of(grid[i], [](int i) { return i == 0; })) {
+            expansion_points.first.push_back(i);
+        }
+    }
+
+    // find empty columns
+    for (int i = 0; i < grid[0].size(); ++i) {
+        if (std::ranges::all_of(grid, [i](const std::vector<int> &row) { return row[i] == 0; })) {
+            expansion_points.second.push_back(i);
+        }
+    }
+
+    return expansion_points;
+}
+
+uint64_t sum_shortest_distances(const std::vector<std::vector<int>> &grid,
+                                const std::pair<std::vector<size_t>, std::vector<size_t>> &expansion_points) {
+    uint64_t sum = 0;
+
+    std::vector<std::pair<int, int>> galaxy_positions{};
+
+    for (int row = 0; row < grid.size(); ++row) {
+        for (int col = 0; col < grid[row].size(); ++col) {
+            if (grid[row][col] != 0) {
+                galaxy_positions.emplace_back(row, col);
+            }
+        }
+    }
+
+    for (int i = 0; i < galaxy_positions.size(); ++i) {
+        for (int j = i + 1; j < galaxy_positions.size(); ++j) {
+            auto [row1, col1] = galaxy_positions[i];
+            auto [row2, col2] = galaxy_positions[j];
+
+            auto row_expansions_between = std::ranges::count_if(expansion_points.first, [row1, row2](size_t row) { return row1 < row && row < row2; });
+
+            auto col_expansions_between = std::ranges::count_if(expansion_points.second, [col1, col2](size_t col) { return (col1 < col && col < col2) || (col2 < col && col < col1); });
+
+//            if (i == 0 && j == 2) {
+//                fmt::println("Expansion points: ({}, {})", row_expansions_between, col_expansions_between);
+//            }
+//
+//            fmt::println("Distance between ({}, {}) is {}", i, j, std::abs(row1 - row2) + std::abs(col1 - col2));
+
+            sum += std::abs(row1 - row2) + (row_expansions_between * EXPANSION_SIZE) + std::abs(col1 - col2) + (col_expansions_between * EXPANSION_SIZE);
         }
     }
 
--- puzzle.h
+++ puzzle.h
@@ -19,19 +19,24 @@
 
 #include "fmt/core.h"
 
+constexpr uint64_t EXPANSION_SIZE = 999'999;
+
 int puzzle_sample_1(const std::string &base_file_path);
 
-int puzzle_sample_2(const std::string &base_file_path);
+uint64_t puzzle_sample_2(const std::string &base_file_path);
 
 int puzzle_1(const std::string &base_file_path);
 
-int puzzle_2(const std::string &base_file_path);
+uint64_t puzzle_2(const std::string &base_file_path);
 
 int do_puzzle_1(std::ifstream &file);
 
-int do_puzzle_2(std::ifstream &file);
+uint64_t do_puzzle_2(std::ifstream &file);
 
 void expand_universe(std::vector<std::vector<int>> &grid);
 int sum_shortest_distances(std::vector<std::vector<int>> &grid);
+
+std::pair<std::vector<size_t>, std::vector<size_t>> find_expansion_points(const std::vector<std::vector<int>> &grid);
+uint64_t sum_shortest_distances(const std::vector<std::vector<int>> &grid, const std::pair<std::vector<size_t>, std::vector<size_t>> &expansion_points);
 
 #endif //PUZZLE_H
